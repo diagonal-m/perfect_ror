e4b740307c08d8d53a32b609b2c9586063ab038e
佐藤太一 <taichi@taichi.local>
2022-05-22 21:41:13 +0900

    2-2-2

---
+++ book_admin/book_admin/app/models/book.rb
@@ -0,0 +1,9 @@
+class Book < ApplicationRecord
+  scope :costry, -> { where("price > ?", 3000) }
+  scope :written_about, -> (theme) { where("name like ?", "%#{theme}%") }
+  scope :find_price, -> (price) { find_by(price: price) }
+
+  belongs_to :publisher
+  has_many :book_authors
+  has_many :authors, through: :book_authors
+end
